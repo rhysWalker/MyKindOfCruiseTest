fbcb078158f2e0a8804ebce0e2f70ffa0e6fcb07
rhysWalker <rhyswalker360@hotmail.com>
2024-11-04 10:13:54 +0000

    cruise and adventure data

---
+++ src/data/adventures.ts
@@ -0,0 +1,54 @@
+const adventures = [
+  {
+    name: "P&O Cruise + Flights",
+    image:
+      "https://images.ctfassets.net/1roij4s04al1/3FPlMGcivSjq16hWSUXG56/28833a1d2858cadb3cfb3f006e9c3956/test4.jpg",
+  },
+  {
+    name: "Princess Cruises",
+    image:
+      "https://images.ctfassets.net/1roij4s04al1/2na8tUef7fGlNBRiw6RFHd/0b47c91e0eba85f364916e1de50cd06c/cabo-san-lucas-mexico-el-arco-couple-boat-excursion_1x1-Square.jpeg",
+  },
+  {
+    name: "Deal of the Week",
+    image:
+      "https://images.ctfassets.net/1roij4s04al1/56DfehbvExzdxWD4kQrW8f/9f968d6728d46ddec12f6f41e0e0b5b5/Blue_Illustrative_Summer_Vacation_Ideas_Presentation__2_.gif",
+  },
+  {
+    name: "Late Deals",
+    image:
+      "https://images.ctfassets.net/1roij4s04al1/58PksAnlqKtgieTtMpStlm/e4ed32f609305739d349b6e57a6c88b4/Blue_Neon_Black_Friday_Sale_Instagram_Story.gif",
+  },
+  {
+    name: "MSC Cruise Deals",
+    image:
+      "https://images.ctfassets.net/1roij4s04al1/46Mc59ndG79eoA52WrBGnb/0d989b93ea28c0459b98c66c940c78fb/MSC_Cruise_deals__6_.jpg",
+  },
+  {
+    name: "P&O Cruises",
+    image:
+      "https://images.ctfassets.net/1roij4s04al1/3PzCRa7o1zQR9zQTt3xcXK/d0a2cde5b991b5a3a20fb2aab1e6663e/Arvia-render.jpg",
+  },
+  {
+    name: "Ambassador £1 Deposit",
+    image:
+      "https://images.ctfassets.net/1roij4s04al1/6NXDaDsmAWGCuIDk1T3n1L/980ae11d595de48c1118397dff8bfd03/ambitionexternal.jpg",
+  },
+  {
+    name: "Royal Caribbean ",
+    image:
+      "https://images.ctfassets.net/1roij4s04al1/3u2JgeAvVXdZj9HpxStrNY/b7004bff55f3424bfce8cfdb66773a14/anthem-north-star-hero.jpeg.jpeg",
+  },
+  {
+    name: "Virgin Voyages",
+    image:
+      "https://images.ctfassets.net/1roij4s04al1/7njJmIqOSIx5DBq11rmDIW/915aa85d72a4f7cc965d3aab9ed02fe7/valiant-lady-sunset__1_.jpg",
+  },
+  {
+    name: "Celebrity Cruises",
+    image:
+      "https://images.ctfassets.net/1roij4s04al1/4xHbkdTkAgYr6O4mTp5zHL/9706bed6fe7a46c08f5e6001db75651e/CEL_Fleet_Image_Horizontal.jpg",
+  },
+];
+
+export default adventures;
