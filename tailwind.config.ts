4a4911108e6de0e2ae1110cfb440162c723213cf
rhysWalker <rhyswalker360@hotmail.com>
2024-11-04 10:01:13 +0000

    add mykindofcruise blue to tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        cruiseBlue: "#0056a2",
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
