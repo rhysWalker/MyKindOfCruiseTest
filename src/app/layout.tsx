b010b216ddee43a435d812f2ea109ee99688763a
rhysWalker <rhyswalker360@hotmail.com>
2024-11-04 10:10:18 +0000

    gut default page, create header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import "./globals.css";
 import { Outfit } from "next/font/google";
+import Image from "next/image";
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -14,7 +16,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={outfit.className}>
-      <body>{children}</body>
+      <body>
+        <header className="bg-cruiseBlue h-24 rounded-b-lg flex items-center">
+          <Link href={"/"}>
+            <Image
+              src={"/logo.png"}
+              alt="My Kind Of Cruise Logo"
+              height={100}
+              width={200}
+              className="ml-32"
+            />
+          </Link>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
