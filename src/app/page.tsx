b7822aae6c4372812be1756f858669671b37d083
rhysWalker <rhyswalker360@hotmail.com>
2024-11-04 10:33:06 +0000

    my adventures section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,31 @@ export default function Home() {
   const adventureList = adventures;
   const cruiseList = cruises;
   return (
-    <div className="">
-      <div>
-        {adventureList.map((adventure, i) => (
-          <div>
-            <p>{adventure.name}</p>
-            <Image
-              src={adventure.image}
-              height={100}
-              width={100}
-              alt="adventure image"
-            />
+    <div className="px-32 mb-10">
+      <main className="grid space-y-10">
+        <div>
+          <h2 className="font-bold py-10 text-lg">My Adventures</h2>
+          <div className="grid grid-cols-5 gap-4">
+            {adventureList.map((adventures, i) => (
+              <div key={i} className="grid gap-2 border rounded-lg shadow pb-4">
+                <div className="flex justify-center">
+                  <Image
+                    className="rounded-lg"
+                    src={adventures.image}
+                    width={200}
+                    height={200}
+                    alt="Adventure Image"
+                  />
+                </div>
+                <div className="flex justify-center">
+                  <p className="font-bold">{adventures.name}</p>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+        <div>Cruises Section</div>
+      </main>
     </div>
   );
 }
