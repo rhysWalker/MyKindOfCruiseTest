de593addcdbc23019b79bd39ca9076cc212447e0
rhysWalker <rhyswalker360@hotmail.com>
2024-11-04 10:42:17 +0000

    cruises section, add image perms to config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,39 @@ export default function Home() {
             ))}
           </div>
         </div>
-        <div>Cruises Section</div>
+        <div>
+          <h2 className="font-bold py-10 text-lg">Popular Cruises</h2>
+          <div className="grid grid-cols-5 gap-4">
+            {cruiseList.map((cruises, i) => (
+              <div key={i} className="grid gap-2 border rounded-lg shadow pb-4">
+                <div className="flex justify-center">
+                  <Image
+                    className="rounded-lg"
+                    src={cruises.shipImage}
+                    width={200}
+                    height={200}
+                    alt=""
+                  />
+                </div>
+                <div className="px-2 text-sm">
+                  <p className="font-bold">
+                    {cruises.nights} nights {cruises.name}
+                  </p>
+                  <p className="">{cruises.shipName}</p>
+                  <div className="">
+                    From <div className="font-bold">{cruises.startPort}</div>
+                  </div>
+                  <div className="">
+                    To <div className="font-bold">{cruises.endPort}</div>
+                  </div>
+                  <div className="pt-2">
+                    <p className="">£{cruises.price}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </main>
     </div>
   );
