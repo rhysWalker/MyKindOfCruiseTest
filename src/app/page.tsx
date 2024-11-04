8e17670bac95f137533fbb4e53b83f73019594e3
rhysWalker <rhyswalker360@hotmail.com>
2024-11-04 10:57:53 +0000

    adventure section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import adventures from "@/data/adventures";
 import cruises from "@/data/cruises";
 import Image from "next/image";
+import AdventureComponent from "./components/adventure";
 
 export default function Home() {
   const adventureList = adventures;
@@ -8,27 +9,7 @@ export default function Home() {
   return (
     <div className="px-32 mb-10">
       <main className="grid space-y-10">
-        <div>
-          <h2 className="font-bold py-10 text-lg">My Adventures</h2>
-          <div className="grid grid-cols-5 gap-4">
-            {adventureList.map((adventures, i) => (
-              <div key={i} className="grid gap-2 border rounded-lg shadow pb-4">
-                <div className="flex justify-center">
-                  <Image
-                    className="rounded-lg"
-                    src={adventures.image}
-                    width={200}
-                    height={200}
-                    alt="Adventure Image"
-                  />
-                </div>
-                <div className="flex justify-center">
-                  <p className="font-bold">{adventures.name}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        <AdventureComponent />
         <div>
           <h2 className="font-bold py-10 text-lg">Popular Cruises</h2>
           <div className="grid grid-cols-5 gap-4">
